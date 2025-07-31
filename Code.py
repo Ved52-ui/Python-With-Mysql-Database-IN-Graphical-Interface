3f61c7350f4cd997e718e12772fbe01d8af16570
Ved Patel <137433618+Ved52-ui@users.noreply.github.com>
2025-07-31 14:48:21 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -3,7 +3,7 @@ from tkinter import messagebox
 from tkinter import ttk
 import mysql.connector
 
-# Funcrogkkrgvkkgvnolvkvjvkkvnkc
+# Funcrogkkrgvkkgvnolvk
 def submit_data():
     name = entry_name.get()
     number = entry_number.get()
